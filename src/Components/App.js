90ad262e490327889e8afcd354b3f5f5167604c0
Ryson Shalvis <rysonshalvis@Rysons-MacBook-Pro.local>
2020-10-10 19:58:29 -0600

    add most static components

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import './App.css';
+import SearchBar from './SearchBar';
+import SearchResults from './SearchResults';
+import Playlist from './PlayList'; 
 
 class App extends React.Component {
   render() {
@@ -7,10 +10,10 @@ class App extends React.Component {
       <div>
     <h1>Ja<span className="highlight">mmm</span>ing</h1>
     <div className="App">
-      {/*<!-- Add a SearchBar component -->*/}
+      <SearchBar />
       <div className="App-playlist">
-        {/*<!-- Add a SearchResults component --> */}
-        {/*<!-- Add a Playlist component -->// */}
+        <SearchResults />
+        <Playlist />
       </div>
     </div>
   </div>
